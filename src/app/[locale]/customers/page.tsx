7b9260272ec333a53384b646ca3d00450213d330
wenjianli <wenjianli@umail.ucsb.edu>
2025-08-07 00:09:24 -0700

    provider page

---
+++ src/app/[locale]/customers/page.tsx
@@ -0,0 +1,10 @@
+import Header from "@/app/components/header";
+import React from "react";
+
+export default function CustomersPage() {
+    return (
+        <div>
+            <Header/>
+        </div>
+    );
+}
